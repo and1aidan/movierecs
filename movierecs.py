afe9e89776dca36f009775eeab508b698023749f
Aidan Alexander <alexanderaidan12@gmail.com>
2025-05-01 03:18:13 -0400

    first data proccesses

--- movierecs.py
+++ movierecs.py
@@ -1,1 +1,10 @@
-print("test")
+import pandas as pd
+
+#read data from each csv file
+movies_data = pd.read_csv('C:/Coding/movierec/movierecs/ml-latest-small/movies.csv')
+ratings_data = pd.read_csv('C:/Coding/movierec/movierecs/ml-latest-small/ratings.csv')
+tags_data = pd.read_csv('C:/Coding/movierec/movierecs/ml-latest-small/tags.csv')
+#create new matrix using pivot_table for more data processing
+ratings_agg = ratings_data.pivot_table(index='userId',columns='movieId',values='rating')
+
+print(ratings_agg.shape)
